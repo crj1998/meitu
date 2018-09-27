54f383b7edd5601f3fc8b46e6440939a7a240d79
crj1998 <crj1998@sjtu.edu.cn>
2018-09-27 19:48:50 +0800

    修复了文件夹Bug

--- meituCore.py
+++ meituCore.py
@@ -101,18 +101,18 @@ class meituInfoThread(QThread):
 class meituDownThread(QThread):
     download_process_signal = pyqtSignal(int)
 
-    def __init__(self, root,filedata):
+    def __init__(self, root, filedata):
         super().__init__()
         self.filename = filedata[0]
         self.imgdict = filedata[1]
-        self.root = os.path.join(root,"Picture")
+        self.__root = os.path.join(root,"Picture")
 
     def savePic(self, referer, url):
         headers= {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36","Referer": "https://www.meituri.com/a/10068/"}
-        path=os.path.join(self.root,url.split('/')[-1])
+        path=os.path.join(self.__root,url.split('/')[-1])
         try:
-            if not os.path.exists(self.root):
-                os.mkdir(self.root)
+            if not os.path.exists(self.__root):
+                os.mkdir(self.__root)
             if not os.path.exists(path):
                 r=requests.get(url, headers=headers)
                 with open(path,'wb') as f:
@@ -121,13 +121,15 @@ class meituDownThread(QThread):
                     logging.info("Picture @ %s saved Successfully!(Referer: %s)"%(url,referer))
             else:
                 logging.warning("File %s Existed"%path)
-        except:
-            logging.warning("Picture @ %s Failed!(Referer: %s)"%(url,referer))
+        except Exception as e:
+            logging.warning("Picture @ %s Failed!(Referer: %s) for %s"%(url,referer,e))
 
     def run(self):
         try:
             process = 0
-            self.root = os.path.join(self.root,self.filename)
+            if not os.path.exists(self.__root):
+                os.mkdir(self.__root)
+            self.__root = os.path.join(self.__root,self.filename)
             for r,urls in self.imgdict.items():
                 for url in urls:
                     process += 1
